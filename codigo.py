c851720ed6308d5d70e3d365250fba6926470b38
albabernal03 <ALBABERO@MYUAX.COM>
2023-09-22 18:50:43 +0200

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,7 +2,9 @@
 import sympy as sym 
 from sympy import symbols 
 from sympy.plotting import plot
+
 f1 = "5+0*x"
 f2 = "8-2*x"
+f3 ='(10-4*x)/5'
 x = symbols('x') 
-plot(f1, f2, (x, 0, 5))
+plot(f1, f2,f3, (x, 0, 5))
